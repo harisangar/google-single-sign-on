a5950ff969d2d91002374f706492eb2603890422
harisangar <ganambihai10@gmail.com>
2022-09-23 17:24:51 +0530

    google signin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect,useState } from 'react';
+import jwt_decode from "jwt-decode";
+
 
 function App() {
+  const [user,setUser]=useState({});
+  const google= window.google;
+
+
+function handleCallbackResponse(response){
+    console.log("encoded JWT ID Token:" + response.credential);
+    var userObject= jwt_decode(response.credential)
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden=true;
+}
+
+function handleSignOut(event){
+  setUser({});
+  document.getElementById("signInDiv").hidden=false;
+}
+useEffect(()=>{
+  google.accounts.id.initialize({
+    client_id:"851224163141-npqc8eencai89027ir5jfb15rk0hcpv6.apps.googleusercontent.com",
+    callback: handleCallbackResponse
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("signInDiv"),
+    {theme:"outline",size:"large"}
+    );
+  //google.accounts.id.prompt();
+},[user]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div id="signInDiv" className='m-5 z-10'></div>
+    
+     { Object.keys(user).length !=0 &&
+      <button  className="m-5 bg-white w-auto hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={(e)=>handleSignOut(e)}>Sign Out</button>
+     }
+
+    {user ?
+      <div className="rounded  shadow-md mt-10 p-12">
+        <img className=" rounded"src={user.picture}></img>
+        <h3>{user.name}</h3>
+      </div>
+      :null
+    }
+      
+     
+     
+     
+      
+
+     
+    
+
+    
+</div>
+
+
+
+    
   );
 }
 
